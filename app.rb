f088be65ce30ce1487fb70d82fdfb7af39d47cdf
Ian Vlahovic <banddorkfht7@gmail.com>
2018-07-05 17:46:48 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -12,11 +12,12 @@ class App < Sinatra::Base
 
   get '/say/:number/:phrase' do
     params[:number].to_i.times do
-      params[:phrase]
+      "#{params[:phrase]}"
     end
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
+
   end
 
 end
